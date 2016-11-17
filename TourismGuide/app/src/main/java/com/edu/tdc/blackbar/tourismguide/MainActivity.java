d6b4136e790134672661913f2289c9a6b0697e37
Shiro <a.cloud.blue@gmail.com>
2016-11-17 22:32:42 +0700

    First Create TourismGuide Project by Shiro - 17/11/2016 - 10h pm

---
+++ TourismGuide/app/src/main/java/com/edu/tdc/blackbar/tourismguide/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edu.tdc.blackbar.tourismguide;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+    }
+}
